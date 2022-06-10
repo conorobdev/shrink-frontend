6198c45b759cc53f24dec9ca0e963b35af59ab23
conorobdev <obrienc49@gmail.com>
2022-06-10 10:19:14 +0100

    Styling changes.

--- src/Home.js
+++ src/Home.js
@@ -15,15 +15,16 @@ const Home = () => {
     return (
         <div>
             <Navbar bg="primary" variant="dark">
-                <h1 style={{ color: "white", textAlign: "center" }}>Shrink</h1>
+                <h1 style={{ color: "white", margin: "0 auto"}}>Shrink</h1>
             </Navbar>
-            <Form.Label htmlFor="urlInput">URL</Form.Label>
+            <Form.Label htmlFor="urlInput" style={{ marginTop: "1%" }}>URL</Form.Label>
             <Form.Control
                 type="text"
                 id="urlInput"
                 aria-describedby="urlTextBox"
+                style={{ width: "50%", margin: "0 auto"}}
             />
-            <Button variant="primary" onClick={startUrlShortService}>Get New Url</Button>
+            <Button variant="primary" onClick={startUrlShortService} style={{ marginTop: "1%", marginBottom: "1%"}}>Get New Url</Button>
             {serviceResponse}
         </div>
     );
--- src/scripts/GetNewUrl.js
+++ src/scripts/GetNewUrl.js
@@ -2,12 +2,12 @@ import axios from "axios"
 
 const GetNewUrl = async (url) => {
     let encodedUrl = encodeURI(url);
-
+    const Url = "http://localhost:3000/sk/";
     return new Promise(((resolve) => {
         axios.post('http://localhost:8080/store', {
             userUrl: encodedUrl
         }).then((response) => {
-            resolve(<p>Success your new url is http://localhost:3000/sk/{response.data}</p>)
+            resolve(<p>Success your new url is <a href={Url + response.data}>http://localhost:3000/sk/{response.data}</a></p>)
         });
     }))
 };
